d6bfca22085344d55ecd6a0db60a7acf05795622
Mikhailov Anton <anion155@gmail.com>
2025-12-05 12:23:06 +0400

    feat: env config

---
+++ eslint.config.js
@@ -0,0 +1,52 @@
+import comments from "@eslint-community/eslint-plugin-eslint-comments/configs";
+import js from "@eslint/js";
+import prettier from "eslint-config-prettier/flat";
+import { defineConfig, globalIgnores } from "eslint/config";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser },
+  },
+  prettier,
+  comments.recommended,
+  globalIgnores(["coverage/"]),
+
+  tseslint.configs.recommendedTypeChecked.map((config) => ({ ...config, files: config.files ?? ["**/*.{ts,tsx}"] })),
+  {
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: { parserOptions: { projectService: true } },
+  },
+  {
+    files: ["**/*.{ts,tsx}"],
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          args: "after-used",
+          argsIgnorePattern: "^_",
+          caughtErrors: "all",
+          caughtErrorsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          ignoreRestSiblings: true,
+        },
+      ],
+      "@typescript-eslint/unbound-method": "off",
+      "@typescript-eslint/no-unsafe-declaration-merging": "off",
+      "@typescript-eslint/no-empty-object-type": ["error", { allowInterfaces: "with-single-extends" }],
+      "@typescript-eslint/consistent-type-imports": ["warn"],
+      "@typescript-eslint/only-throw-error": "off",
+    },
+  },
+  {
+    files: ["**/*.{test,spec}-d.{ts,tsx}"],
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+    },
+  },
+]);
